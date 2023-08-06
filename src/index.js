aa5c3a54d6aba6d83929b1db4ef7252d2f707bf6
jbalaraman <james.balaraman@gmail.com>
2023-08-06 17:22:27 -0600

    I dont understand this at all and I really dont like the videos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,73 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+let listItemCounter = 1;
+let allBreeds = {};
+
+function dogPics() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(json => renderImg(json));
+}
+
+function dogBreeds() {
+    const breedUrl = "https://dog.ceo/api/breeds/list/all";
+    fetch(breedUrl)
+        .then(resp => resp.json())
+        .then(json => {
+            allBreeds = json.message;
+            renderList(allBreeds);
+        });
+
+    const breedDropdown = document.getElementById('breed-dropdown');
+    breedDropdown.addEventListener('change', () => {
+        const selectedLetter = breedDropdown.value;
+        filterBreedsByLetter(selectedLetter);
+    });
+}
+
+function renderImg(images) {
+    const loc = document.getElementById('dog-image-container');
+    loc.innerHTML = '';
+    images.message.forEach(Image => {
+        const img = document.createElement('img');
+        img.src = Image;
+        loc.appendChild(img);
+    });
+}
+
+function renderList(breeds) {
+    const what = document.getElementById('dog-breeds');
+    what.innerHTML = '';
+    for (const breed in breeds) {
+        const breedItem = document.createElement('li');
+        breedItem.textContent = breed;
+        breedItem.id = 'list-item-' + listItemCounter++;
+        what.appendChild(breedItem);
+        breedItem.addEventListener('click', colorSwitch);
+    }
+}
+
+function filterBreedsByLetter(letter) {
+    const filteredBreeds = Object.keys(allBreeds).filter(breed =>
+        breed.toLowerCase().startsWith(letter.toLowerCase())
+    );
+
+    const what = document.getElementById('dog-breeds');
+    what.innerHTML = '';
+    filteredBreeds.forEach(breed => {
+        const breedItem = document.createElement('li');
+        breedItem.textContent = breed;
+        breedItem.id = 'list-item-' + listItemCounter++;
+        what.appendChild(breedItem);
+        breedItem.addEventListener('click', colorSwitch);
+    });
+}
+
+function colorSwitch() {
+    this.style.color = 'orange';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    dogPics();
+    dogBreeds();
+});
